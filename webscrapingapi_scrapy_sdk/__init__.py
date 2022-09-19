e2fb45903f24c191b6ec998b4afb7b8e61b32dd0
Djordje Puzic <djordjepuzic@gmail.com>
2022-09-19 13:23:18 +0200

    first commit

---
+++ webscrapingapi_scrapy_sdk/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['WebScrapingApiRequest', 'WebScrapingApiMiddleware', 'WebScrapingApiSpider']
+
+from web_scraping_scrapy_sdk.request import WebScrapingApiRequest
+from web_scraping_scrapy_sdk.middleware import WebScrapingApiMiddleware
+from web_scraping_scrapy_sdk.spider import WebScrapingApiSpider
